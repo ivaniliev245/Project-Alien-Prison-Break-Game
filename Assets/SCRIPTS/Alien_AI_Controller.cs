162928a325c2820bc9e11ae3dddee646f56ced39
ivaniliev245 <ivaniliev245@gmail.com>
2023-11-20 14:02:51 +0100

    some changes
    some changes

--- Assets/SCRIPTS/Alien_AI_Controller.cs
+++ Assets/SCRIPTS/Alien_AI_Controller.cs
@@ -1,17 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Alien_AI_Controller : MonoBehaviour
 {
     public Transform player; // Reference to the player's Transform
-    public float moveSpeed = 3f; // Speed at which the enemy moves towards the player
+    public float moveSpeed = 2f; // Speed at which the enemy moves towards the player
+    public float rotationSpeed = 2f; // Speed at which the enemy rotates towards the player
 
     private Rigidbody enemyRigidbody;
 
     void Start()
     {
         enemyRigidbody = GetComponent<Rigidbody>(); // Getting the Rigidbody component of the enemy
+
+        // Lock rotation along X and Z axes
+        enemyRigidbody.freezeRotation = true;
     }
 
     void Update()
@@ -21,12 +23,13 @@ public class Alien_AI_Controller : MonoBehaviour
             // Calculate the direction from the enemy to the player
             Vector3 directionToPlayer = (player.position - transform.position).normalized;
 
-            // Move the enemy towards the player
-            enemyRigidbody.MovePosition(transform.position + directionToPlayer * moveSpeed * Time.deltaTime);
+            // Move the enemy towards the player gradually
+            Vector3 targetPosition = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+            enemyRigidbody.MovePosition(targetPosition);
 
-            // Rotate the enemy to face the player (optional)
-            Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
-            enemyRigidbody.MoveRotation(lookRotation);
+            // Rotate the enemy to face the player smoothly
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
