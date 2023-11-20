8d5f90f8ce96dbc2340d307990a696b1ab4bb24e
ivaniliev245 <ivaniliev245@gmail.com>
2023-11-20 21:51:57 +0100

    improved grass shader
    improved grass shader

---
+++ Assets/MATERIALS/Grass/CreateGrass.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGrass : MonoBehaviour
+{
+    
+    public GameObject grassPrefab;
+    public int grassSize= 20;
+    public GameObject character;
+    public Material mat;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+            for (int z=-grassSize; z <= grassSize; z++)
+                for (int x = -grassSize; x <= grassSize; x++)
+            {
+                Vector3 position = new Vector3(x / 4.0f + 
+                                Random.Range (-0.25f, 0.25f), 0, z/4.0f + Random.Range (-0.25f, 0.25f));
+              GameObject grass = Instantiate (grassPrefab, position, Quaternion.identity);
+              grass.transform.localScale = new Vector3(1, Random.Range(0.8f, 1.2f), 1);
+
+            }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       mat.SetVector("_TramplePosition", character.transform.position);
+    }
+}
