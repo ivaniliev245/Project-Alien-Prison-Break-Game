a07b1e08fb58f78cc5103201ddbcfc017dbf5bda
ivaniliev245 <ivaniliev245@gmail.com>
2023-11-22 10:41:44 +0100

    Revert "Merge branch 'main' of https://github.com/ivaniliev245/Project-Alien-Prison-Break-Game"
    This reverts commit 9538cebf033af43eeab00de75818248d60cf2c1e, reversing
    changes made to cc7416e165ed33413c9335917b3d5492977e6957.

--- Assets/SCRIPTS/Alien_AI_Controller.cs
+++ Assets/SCRIPTS/Alien_AI_Controller.cs
@@ -1,26 +1,21 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Alien_AI_Controller : MonoBehaviour
 {
-    public Transform player; // Reference to the player's Transform
-    public float moveSpeed = 2f; // Speed at which the enemy moves towards the player
-    public float rotationSpeed = 2f; // Speed at which the enemy rotates towards the player
+    public NavMeshAgent goblin;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
 
-    private Rigidbody enemyRigidbody;
+    //patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkpointRange;
 
-    void Start()
-    {
-        enemyRigidbody = GetComponent<Rigidbody>(); // Getting the Rigidbody component of the enemy
+    //Attackspeed  
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
 
-<<<<<<< HEAD
-        // Lock rotation along X and Z axes
-        enemyRigidbody.freezeRotation = true;
-    }
-
-    void Update()
-    {
-        if (player != null)
-=======
     //States
     public float sightRange, attackRange;
     bool playerInSightRange, playerInAttackRange;
@@ -57,21 +52,9 @@ public class Alien_AI_Controller : MonoBehaviour
     {
         if (!walkPointSet) { SearchWalkPoint(); }
         else
->>>>>>> 1f344ca0c61191bfcc704af4c56c49f79adde366
         {
-            // Calculate the direction from the enemy to the player
-            Vector3 directionToPlayer = (player.position - transform.position).normalized;
-
-            // Move the enemy towards the player gradually
-            Vector3 targetPosition = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
-            enemyRigidbody.MovePosition(targetPosition);
-
-            // Rotate the enemy to face the player smoothly
-            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            goblin.SetDestination(walkPoint);
         }
-<<<<<<< HEAD
-=======
 
         Vector3 disctanceToWalkPoint = transform.position - walkPoint;
 
@@ -146,6 +129,5 @@ public class Alien_AI_Controller : MonoBehaviour
         Gizmos.DrawSphere(transform.position,attackRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(transform.position, sightRange);
->>>>>>> 1f344ca0c61191bfcc704af4c56c49f79adde366
     }
 }
